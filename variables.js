6dec496fd65fae53d9c34e40adc19e9ebd4c9d0f
TheRudraBro <rudramraj22@gmail.com>
2025-07-29 23:20:37 +0600

    First commit

---
+++ variables.js
@@ -0,0 +1,33 @@
+/*
+variable ==> container
+*/ 
+
+// Number
+var sunglassPrice=90;
+
+// string
+var subject=ErrorEvent;
+
+// boolean
+var passed=true;
+
+/**  Naming Convention
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. cannot start with number
+ * 5. can not be keyword
+ * 6. prefer camelcase
+ */
+
+
+var lastExamResult = 89;
+/**
+ * operation
+ * 1. +,-,*,/,%
+ * 2. +=,-=,*=,/=
+ * 3. isNan?
+ * 4. Number
+ * 
+
+ */
